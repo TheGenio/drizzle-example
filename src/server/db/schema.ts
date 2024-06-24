8046980c02c56081d0d695d39614eebafbc4bdac
TheGenio <s202164170@kfupm.edu.sa>
2024-06-24 19:37:17 +0300

    init

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -12,13 +12,8 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `kfupm-g-todo_${name}`);
+
+export const createTable = pgTableCreator((name) => `testing_${name}`);
 
 export const users = createTable("users", {
   id: uuid('id').defaultRandom().primaryKey(),
